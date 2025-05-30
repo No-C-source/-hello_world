167661e917836f0f9d29c151023415a499d226b8
No-C-source <claire.links2@gmail.com>
2025-05-30 21:00:41 +0200

    Add files via upload

---
+++ hello_world/hello_world.py
@@ -0,0 +1,13 @@
+'''
+assign name to variable name
+display name
+assign age to variable age
+display age
+print hello world
+'''
+
+name = input("What is your name?: ")
+print(name)
+age = input("What is your age?: ")
+print(age)
+print("hello world")
